8e7621d8f250dbe16b53dceba264a6bfa5ab8f60
Mohamed Touzani <mohamed.touzani@gmx.de>
2025-10-15 09:20:00 +0200

    feat: add transloco i18n loader and configure HTTP client

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,28 @@
 import {
   ApplicationConfig,
   provideZoneChangeDetection,
+  isDevMode,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { provideTransloco } from '@jsverse/transloco';
+import { TranslocoHttpLoader } from './transloco-loader';
+
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideHttpClient(),
+    provideTransloco({
+      config: {
+        availableLangs: ['de', 'en'],
+        defaultLang: 'en',
+        reRenderOnLangChange: true,
+        prodMode: !isDevMode(),
+      },
+      loader: TranslocoHttpLoader,
+    }),
   ],
 };
