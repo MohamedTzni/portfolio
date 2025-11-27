130570da515de08f716abcc490d2da90c93493f0
Mohamed Touzani <mohamed.touzani@gmx.de>
2025-11-27 14:40:00 +0100

    feat: add routing configuration for main, legal-notice and privacy-policy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
-import { Routes } from '@angular/router';
+﻿import { Routes } from '@angular/router';
+import { MainPage } from './main/main-page/main-page';
+import { LegalNotice } from './shared/legal-notice/legal-notice';
+import { PrivacyPolicy } from './shared/privacy-policy/privacy-policy';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainPage,
+  },
+  {
+    path: 'legal-notice',
+    component: LegalNotice,
+  },
+  {
+    path: 'privacy-policy',
+    component: PrivacyPolicy,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
