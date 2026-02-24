7b139874e1fca12f4374e0fddb6dc3e1524d143f
Mohamed Touzani <mohamed.touzani@gmx.de>
2026-02-24 10:10:00 +0100

    feat: implement footer with RouterLink and Transloco imports

--- src/app/shared/footer/footer.ts
+++ src/app/shared/footer/footer.ts
@@ -1,8 +1,13 @@
-import { Component } from '@angular/core';
+﻿import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { TranslocoPipe } from '@jsverse/transloco';
+import { Logo } from '../header/logo/logo';
 
 @Component({
   standalone: true,
   selector: 'app-footer',
-  template: '<footer></footer>',
+  imports: [Logo, RouterLink, TranslocoPipe],
+  templateUrl: './footer.html',
+  styleUrl: './footer.scss',
 })
 export class Footer {}
