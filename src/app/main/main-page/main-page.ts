54f2f9ec0ea037d6df8754cb81427faf72094a9e
Mohamed Touzani <mohamed.touzani@gmx.de>
2026-03-07 16:10:00 +0100

    feat: update main-page to compose all section components

--- src/app/main/main-page/main-page.ts
+++ src/app/main/main-page/main-page.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+﻿import { Component } from '@angular/core';
+import { Hero } from '../hero/hero';
+import { AboutMe } from '../about-me-section/about-me-section';
+import { Skills } from '../skills/skills';
+import { Projects } from '../projects/projects';
+import { References } from '../references/references';
+import { Contact } from '../contact/contact';
+import { Footer } from '../../shared/footer/footer';
 
 @Component({
-  standalone: true,
   selector: 'app-main-page',
-  template: '<main></main>',
+  standalone: true,
+  imports: [Hero, AboutMe, Skills, Projects, References, Contact, Footer],
+  templateUrl: './main-page.html',
+  styleUrl: './main-page.scss',
 })
 export class MainPage {}
