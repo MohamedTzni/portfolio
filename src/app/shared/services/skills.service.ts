1ded988c5b7aa917c2b787b8a3b9b2b08f5a1e97
Mohamed Touzani <mohamed.touzani@gmx.de>
2026-05-07 11:15:03 +0200

    feat: add Python, Django, SQL, PostgreSQL, Docker, Linux, Cloud skill icons

--- src/app/shared/services/skills.service.ts
+++ src/app/shared/services/skills.service.ts
@@ -17,6 +17,13 @@ export class SkillsService {
     { label: 'REST-API', icon: 'assets/icon/skill-icon/api_icon.png' },
     { label: 'Scrum', icon: 'assets/icon/skill-icon/scrum_icon.png' },
     { label: 'Growth mindset', icon: 'assets/icon/skill-icon/mindset_icon.png' },
+    { label: 'Python', icon: 'assets/icon/skill-icon/Python.png' },
+    { label: 'Django', icon: 'assets/icon/skill-icon/Django.png' },
+    { label: 'SQL', icon: 'assets/icon/skill-icon/SQL.png' },
+    { label: 'PostgreSQL', icon: 'assets/icon/skill-icon/PostgreSQL.png' },
+    { label: 'Docker', icon: 'assets/icon/skill-icon/Docker.png' },
+    { label: 'Linux', icon: 'assets/icon/skill-icon/Linux.png' },
+    { label: 'Cloud', icon: 'assets/icon/skill-icon/Cloud.png' },
   ];
 
   /**
